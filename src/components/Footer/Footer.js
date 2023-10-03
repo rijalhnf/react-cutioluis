fc538923a3be7f34eef2fffda740280c7d579ac0
rijalhnf <rhizeft@gmail.com>
2023-10-03 21:09:58 +0700

    ok

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,12 @@
+import "./Footer.css";
+
+export const Footer = () => {
+  return (
+    <footer className="container-footer">
+      <p>
+        Meet me <span className="white-text">Github</span> o{" "}
+        <span className="white-text">Behance</span>
+      </p>
+    </footer>
+  );
+};
